52c0b833715d3b54ca3367aa3bdf6dae6a0e390d
Diego Magdaleno <diego.magdaleno@ext.valid.com>
2018-07-03 15:57:24 -0300

    picture form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController, ToastController } from 'ionic-angular';
-import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { DomSanitizer, SafeHtml, SafeStyle } from '@angular/platform-browser';
+
+import { Mock } from '../../app/model/mock'
 
 @Component({
   selector: 'page-home',
@@ -9,70 +11,44 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class HomePage {
 
-  imageBase64:any;
-  imageFileName:any;
+  imageView: SafeHtml;
+  imageReady: Boolean = false;
+
+  name: string = '';
+  documentType: string = '';
+  document: string = '';
+  gender: string = '';
+  imageBase64: string = '';
 
   constructor(
     public navCtrl: NavController,
-    private transfer: FileTransfer,
     private camera: Camera,
     public loadingCtrl: LoadingController,
-    public toastCtrl: ToastController) { }
+    public toastCtrl: ToastController,
+    private sanitizer: DomSanitizer) { }
 
   getImage() {
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
       sourceType: this.camera.PictureSourceType.CAMERA
     }
     this.camera.getPicture(options).then((imageData) => {
       this.imageBase64 = imageData;
+      this.imageView = this.sanitizer
+        .bypassSecurityTrustResourceUrl('data:image/png;base64,' + imageData);
+      this.imageReady = true;
     }, (err) => {
       console.log(err);
-      this.presentToast(err);
+      var mock: Mock = new Mock();
+      this.imageView = this.sanitizer
+        .bypassSecurityTrustResourceUrl(mock.base64);
+      this.imageReady = true;
     });
   }
 
-  uploadFile() {
-    let loader = this.loadingCtrl.create({
-      content: "Uploading..."
-    });
-    loader.present();
-    const fileTransfer: FileTransferObject = this.transfer.create();
-  
-    let options: FileUploadOptions = {
-      fileKey: 'ionicfile',
-      fileName: 'ionicfile',
-      chunkedMode: false,
-      mimeType: "image/jpeg",
-      headers: {}
-    }
-  
-    fileTransfer.upload(this.imageBase64, 'http://192.168.0.7:8080/api/uploadImage', options)
-      .then((data) => {
-      console.log(data+" Uploaded Successfully");
-      this.imageFileName = "http://192.168.0.7:8080/static/images/ionicfile.jpg"
-      loader.dismiss();
-      this.presentToast("Image uploaded successfully");
-    }, (err) => {
-      console.log(err);
-      loader.dismiss();
-      this.presentToast(err);
-    });
+  avancar(){
+    console.log('clicou em avançar');
   }
-
-  presentToast(msg) {
-    let toast = this.toastCtrl.create({
-      message: msg,
-      duration: 3000,
-      position: 'bottom'
-    });
-  
-    toast.onDidDismiss(() => {
-      console.log('Dismissed toast');
-    });
-  
-    toast.present();
-  }
-
 }
