fedfa588484aba091757aa563263c8d400893c6c
Diego Magdaleno <diego.magdaleno@ext.valid.com>
2018-07-04 09:38:50 -0300

    form validation blank field

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController, ToastController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
@@ -20,9 +20,9 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     private camera: Camera,
-    public loadingCtrl: LoadingController,
-    public toastCtrl: ToastController,
-    private sanitizer: DomSanitizer) {
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController,
+    private _sanitizer: DomSanitizer) {
 
       this.picture = {
         base64: '',
@@ -42,24 +42,39 @@ export class HomePage {
     }
     this.camera.getPicture(options).then((imageData) => {
       this.picture.base64 = imageData;
-      this.imageView = this.sanitizer
+      this.imageView = this._sanitizer
         .bypassSecurityTrustResourceUrl('data:image/png;base64,' + imageData);
       this.imageReady = true;
     }, (err) => {
       console.log(err);
       var mock: Mock = new Mock();
       this.picture.base64 = mock.base64;
-      this.imageView = this.sanitizer
+      this.imageView = this._sanitizer
         .bypassSecurityTrustResourceUrl(mock.base64);
       this.imageReady = true;
     });
   }
 
   send(){
-    console.log('picture.base64: ' + this.picture.base64.substr(0,30).concat('...'));
-    console.log('picture.name: ' + this.picture.name);
-    console.log('picture.documentType: ' + this.picture.documentType);
-    console.log('picture.document: ' + this.picture.document);
-    console.log('picture.birthDate: ' + this.picture.birthDate);
+    if(this.isThereBlankField()){
+      this._alertCtrl.create({
+        title: 'Campo obrigatório',
+        subTitle: 'Favor preencher todos os campos',
+        buttons: [{text: 'Ok'}]
+      }).present();
+      return;
+    }
+    
+  }
+
+  private isThereBlankField(): Boolean {
+    if(this.picture.base64 == '' ||
+        this.picture.name  == '' ||
+        this.picture.documentType == '' ||
+        this.picture.document == '' ||
+        this.picture.birthDate == ''){
+          return true;
+    }
+    return false;
   }
 }
