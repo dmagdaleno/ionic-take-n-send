2599b3eaee421e16b791218343e780ae482257ce
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-07-04 14:34:34 -0300

    model to wrapp picture request

---
+++ src/app/model/request-wrapper.ts
@@ -0,0 +1,9 @@
+export interface RequestWrapper {
+    service: string, 
+    document: string,
+    docType: string,
+    image: string,
+    contract: string,
+    product: string,
+    gallery: string
+ }
